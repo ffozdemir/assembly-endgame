2d9d4bededacfa09f32031e5795b2a94cf6aa374
Fetullah Furkan Özdemir <furkanozdemir51@yandex.com.tr>
2025-05-28 14:29:06 +0200

    Add clsx dependency and update styles for keyboard and word sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,48 @@ import { useState } from "react";
 import "./App.css";
 import Language from "./Language";
 import { languages } from "./languages";
+import { clsx } from "clsx";
 
 function App() {
   const [currentWord, setCurrentWord] = useState("react");
 
-   const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const [guessedLetters, setGuessedLetters] = useState([]);
 
-   const alphabetElements = alphabet.split("").map((letter, index) => (
-    <button key={index}>
-      {letter.toUpperCase()}
-    </button>
-  ));
+  const addGuessedLetter = (letter) => {
+    setGuessedLetters((prevLetters) =>
+      prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter]
+    );
+  };
 
-  const letterElements = currentWord.split("").map((letter, index) => (
-    <span key={index}>
-      {letter.toUpperCase()}
-    </span>
-  ));
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  const keyboardElements = alphabet.split("").map((letter) => {
+    const isGuessed = guessedLetters.includes(letter);
+    const isCorrect = isGuessed && currentWord.includes(letter);
+    const isWrong = isGuessed && !currentWord.includes(letter);
+    const className = clsx({
+      correct: isCorrect,
+      wrong: isWrong,
+    });
+
+    return (
+      <button
+        className={className}
+        key={letter}
+        onClick={() => addGuessedLetter(letter)}
+      >
+        {letter.toUpperCase()}
+      </button>
+    );
+  });
+
+  const letterElements = currentWord
+    .split("")
+    .map((letter, index) => (
+      <span key={index}>
+        {guessedLetters.includes(letter) ? letter.toUpperCase() : ""}
+      </span>
+    ));
 
   const languageElements = languages.map((language) => (
     <Language
@@ -44,9 +69,8 @@ function App() {
       </section>
       <section className="language-chips">{languageElements}</section>
       <section className="word">{letterElements}</section>
-      <section className="keyboard" >
-        {alphabetElements}
-      </section>
+      <section className="keyboard">{keyboardElements}</section>
+      <button className="new-game">New Game</button>
     </main>
   );
 }
